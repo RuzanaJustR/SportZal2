633b74ea297cceaf9577e25fdd494877ca8008f3
User <Ruzana-Klop@mail.ru>
2023-11-21 17:24:00 +0500

    add klient

---
+++ Klient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Klient.Views;
+namespace Klient.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            _ClientsUserControl = new ClientsUserControlViewModel();
+            _ClientsUserControl.DataContext = new ClientsUserControlViewModel();
+        }
+        public ClientsUserControlViewModel _ClientsUserControl { get; set; }
+    }
+}
